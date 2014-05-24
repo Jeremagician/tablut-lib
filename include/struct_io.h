4c16e783094d518ddeb1c3d2302be8d70844da38
Needs <needs@mailoo.org>
2014-05-24 03:41:46 +0200

    Documente the structure IO API.

--- include/struct_io.h
+++ include/struct_io.h
@@ -1,6 +1,21 @@
 #ifndef STRUCT_IO_H_
 #define STRUCT_IO_H_
 
+/*
+ * Provide some functions to help reading/writing data to a file or a buffer.
+ * Read or write using network byte order.
+ *
+ * Reading or writing directly to a file suffer from some limitations due to
+ * dynamic fields.  On error, the file is in invalid state and it can be fatal
+ * for files like socket.  This is why reading or writing to a buffer is highly
+ * recommended, even if it's needs a completly different way of programming, it
+ * is often the *rigth* way of doing IO.
+ *
+ * Also, reading or writing directly to a file will be very slow compared to
+ * reading or writing to a buffer.  So this functions are only there because
+ * some applications may not have high requirement for reliability and
+ * performances.
+ */
 
 #include <stdlib.h>
 #include <stdint.h>
@@ -10,7 +25,7 @@
  * Structure generation.
  *
  * An extra field is added for dynamic entries, user application must not
- * access it or modify it.
+ * access it or modify it unless they what they are doing.
  */
 #define BEGIN(name)                                                     \
 	struct name {
@@ -36,38 +51,83 @@
 #undef  END
 
 
-/*
- * Initializer to zero.  Portable application should only use this to initialize
- * the structure to zero.
- *
- * Read a structure from a file, write a structure to a file.
- * Return 0 on failure (with errno set), 1 on success.
- *
- * Read or write the struct from or to a buffer.  Assume the buffer has the
- * correct size.
- *
- * When reading the structure from a buffer or a file, you can put a structure
- * filled with 0, or with a structure already passed to this functions and not
- * already freed.  In the later, the allocated field (if any) can be reused.
- * You can not have any expectation on what will be reused or not and
- * application must considere old buffers as invalid, specially if reference
- * where keep somewhere else.
- * On error, it is guaranteed that data inside the structure remains valid, but
- * as stated just above, pointers may have change.
- *
- * Compute the (packed) size of the given structure.
- *
- * Free the given structure.
- */
 #define BEGIN(name)                                                     \
-	static const struct name name##_ZERO;                           \
+/*
+ * Initializer to zero.  Portable application should only use this to
+ * initialize the structure to zero.
+ */                                                                     \
+static const struct name name##_ZERO;                                   \
                                                                         \
-	int sio_read_##name(struct name *s, int fd);                    \
-	int sio_write_##name(struct name *s, int fd);                   \
-	int sio_frombuf_##name(struct name *s, char *buf);              \
-	void sio_tobuf_##name(struct name *s, char *buf);               \
-	size_t sio_size_##name(struct name *s);                         \
-	void sio_free_##name(struct name *s);
+/*
+ * Read the structure from the given file descriptor.
+ *
+ * The only accepted structure is a structure initialized to zero or a
+ * structure aready passed to this functions or to sio_frombuf().  On the later
+ * case, dynamic field can be invalidated, wich would make every pointer to
+ * them invalid.
+ *
+ * The function block and terminate when the whole structure is read or when an
+ * error occurs.
+ *
+ * On error, the structure contains undefined data.  But you can re-use it
+ * on this function or sio_frombuf.  Even on error some buffers can remains
+ * allocated and must be freed using sio_free.  Most of the time, the file will
+ * be in a undefined state.
+ *
+ * Return 1 on success or 0 on error, with errno set.
+ */                                                                     \
+int sio_read_##name(struct name *s, int fd);                            \
+                                                                        \
+/*
+ * Write the structure to the given file descritor.
+ *
+ * The function block and terminate when the whole structure is write or when an
+ * error occurs.
+ *
+ * On error or on success, the structure remains untouched and completely valid.
+ * However, on error, the file may be in an undefined state.
+ *
+ * Return 1 on success or 0 on error, with errno set.
+ */                                                                     \
+int sio_write_##name(struct name *s, int fd);                           \
+                                                                        \
+/*
+ * Construct the structure from the given buffer.
+ *
+ * The only accepted structure is a structure initialized to zero or a
+ * structure aready passed to this functions or to sio_read().  On the later
+ * case, dynamic field can be invalidated, wich would make every pointer to
+ * them invalid.
+ *
+ * The buffer is never modified during the whole process.
+ *
+ * Return 1 on success or 0 on error, with errno set.
+ */                                                                     \
+int sio_frombuf_##name(struct name *s, char *buf);                      \
+                                                                        \
+/*
+ * Put the structure in the given buffer.
+ *
+ * Assume that the buffer can contains the whole structure.  You can retrieve
+ * the size of the structure with sio_size().  During the whole process the
+ * structure remains valid.
+ */                                                                     \
+void sio_tobuf_##name(struct name *s, char *buf);                       \
+                                                                        \
+/*
+ * Retrieve the size structure, wich is here the size it will take when writed
+ * to a file or a buffer.
+ */                                                                     \
+size_t sio_size_##name(struct name *s);                                 \
+                                                                        \
+/*
+ * Free the given structure.
+ *
+ * Assume that every dynamic fields of the structure is valid and can be freed
+ * using free().  Dynmic field set to NULL are ignored.
+ */                                                                     \
+void sio_free_##name(struct name *s);
+
 #define FIELD_8(name)
 #define FIELD_16(name)
 #define FIELD_STRUCT(name, s)
